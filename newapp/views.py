b28da48ede5cba2cf10206f24d09792f9be56f51
Michael Maranan <michaelmaranan030@gmail.com>
2021-11-20 13:39:37 +0800

    .

--- newapp/views.py
+++ newapp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+db = {
+	"12": {
+		"task": "wash dishes"
+	},
+	"13": {
+		"task": "something else"
+	}
+}
+
+
+def hello(request):
+	return db
+
+# YOUR IDEA DOESNT WORK AT ALL. YOU NEED THE SERIALIZERS MODULE TO DO THIS
